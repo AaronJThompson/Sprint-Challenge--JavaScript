74e99831cf3f1b5452593aa4a7d76063b7cc8669
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-10 08:44:53 +0100

    Advanced Array request 2 finished

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -146,7 +146,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(item => item.animal_name.toLowerCase());
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
